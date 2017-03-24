ba30aab7767b29d094e10d0e5bb3ba88864f49fc
HTWG-AIN <aiabou@htwg-konstanz.de>
2017-03-24 13:57:06 +0100

    teil 1 !

--- Blatt2/src/aqua/blatt2/broker/Broker.java
+++ Blatt2/src/aqua/blatt2/broker/Broker.java
@@ -1,11 +1,16 @@
 package aqua.blatt2.broker;
 
 import java.net.InetSocketAddress;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
+import javax.swing.JOptionPane;
+
+import aqua.blatt2.common.msgtypes.RegisterRequest;
 import aqua.blatt2.common.Properties;
 import aqua.blatt2.common.msgtypes.DeregisterRequest;
 import aqua.blatt2.common.msgtypes.HandoffRequest;
-import aqua.blatt2.common.msgtypes.RegisterRequest;
 import aqua.blatt2.common.msgtypes.RegisterResponse;
 import messaging.Endpoint;
 import messaging.Message;
@@ -14,30 +19,75 @@ public class Broker {
 
 	private static ClientCollection<InetSocketAddress> clients = new ClientCollection<>();
 	private final static String FISH_NAME = "HTWG ";
+	private static final int NUM_OF_THREADS = 3;
 	private static Endpoint endPoint = new Endpoint(Properties.PORT);
 	private static int counter = 0;
+	private static boolean stopRequested = false;
 
 	public Broker() {
 	}
 
-	public static void broker() {
-//		String[] args = {};
-//		Aqualife.main(args);
-		while (true) {
+	public static class BrokerTask implements Runnable {
 
-			Message message = endPoint.blockingReceive();
-
-			if (message.getPayload() instanceof RegisterRequest)
-				register(message.getSender());
-			if (message.getPayload() instanceof DeregisterRequest)
-				deregister((DeregisterRequest) message.getPayload());
-			if (message.getPayload() instanceof HandoffRequest)
-				handoffFish(message.getSender(), (HandoffRequest) message.getPayload());
+		private static Message message;
+ 
+		public BrokerTask(Message message) {
+			BrokerTask.message = message;
 
 		}
 
+		@Override
+		public void run() {
+ 			playloadMessages(message);
+		}
+
+	}
+	public static class Threads extends Thread{
+		public Threads() {
+ 		}
+		public void run(){
+			System.out.println("bin da");
+			int value = JOptionPane.showConfirmDialog(null, "Do you want to end the Thread ? " ,"End", JOptionPane.YES_NO_CANCEL_OPTION);
+				if (value ==  JOptionPane.YES_OPTION) {
+					System.out.println("wir gesetzt");
+ 					 stopRequested = true;
+				}
+
+		}
+	}
+	private static void playloadMessages(Message message) {
+
+		if (message.getPayload() instanceof RegisterRequest)
+			register(message.getSender());
+		if (message.getPayload() instanceof DeregisterRequest)
+			deregister((DeregisterRequest) message.getPayload());
+		if (message.getPayload() instanceof HandoffRequest)
+			handoffFish(message.getSender(), (HandoffRequest) message.getPayload());
 	}
 
+	public static void broker() throws InterruptedException {
+		Threads threads = new Threads();
+		threads.start();
+		TimeUnit.SECONDS.sleep(1);
+		ExecutorService executor = Executors.newFixedThreadPool(NUM_OF_THREADS);
+		Runnable runner;
+		
+		
+		System.out.println("hier bin ich " + stopRequested);
+		if (!stopRequested) {
+			while (true) {
+
+				Message message = endPoint.blockingReceive();
+				runner = new BrokerTask(message);
+				executor.execute(runner);
+
+			}
+		}
+		executor.shutdown();
+ 
+	}
+
+	 
 	private static void register(InetSocketAddress broker) {
 
 		String id = FISH_NAME + counter;
@@ -54,7 +104,6 @@ public class Broker {
 		if (index != -1)
 			clients.remove(index);
 
-
 	}
 
 	private static void handoffFish(InetSocketAddress broker, HandoffRequest handoffRequest) {
@@ -69,9 +118,9 @@ public class Broker {
 		endPoint.send(broker, handoffRequest);
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		broker();
-		
+
 	}
 
 }
